e636dd8e59b1122769ff1025f36bffe0d8e58043
knipknap <knipknap>
2009-01-06 22:58:43 +0000

    Fix: The indexbar rework earlier broke the search plugin.

--- src/plugins/search/indexbar_search_result.class.php
+++ src/plugins/search/indexbar_search_result.class.php
@@ -29,88 +29,18 @@
     // Constructor.
     function IndexBarSearchResult($_args) {
       $this->Menu();
-      $this->forum_id            = $_args[forum_id];
-      $this->query               = $_args[query];
-      $this->n_postings          = $_args[n_postings];
-      $this->n_postings_per_page = $_args[n_postings_per_page];
-      $this->n_offset            = $_args[n_offset];
-      $this->n_pages_per_index   = $_args[n_pages_per_index];
 
-      // Print the "Index" keyword, followed by a separator.
-      $additem = array(&$this, 'add_item');
-      call_user_func($additem, lang("index"));
-
-      // Calculate the total number of pages.
-      $n_pages = ceil($this->n_postings / $this->n_postings_per_page);
-      if ($n_pages <= 0)
-        $n_pages = 1;
-
-      // Find the selected page's number from the parent with the given offset.
-      $activepage = ceil($this->n_offset / $this->n_postings_per_page) + 1;
-
-      // Find the first number to show in the index.
-      $n_indexoffset = 1;
-      if ($activepage > $this->n_pages_per_index / 2)
-        $n_indexoffset = $activepage - ceil($this->n_pages_per_index / 2);
-      if ($n_indexoffset + $this->n_pages_per_index > $n_pages)
-        $n_indexoffset = $n_pages - $this->n_pages_per_index;
-      if ($n_indexoffset < 1)
-        $n_indexoffset = 1;
-
-      // Always show a link to the first page.
-      $url = new URL('?', cfg("urlvars"));
+      // Prints the index (pagination).
+      $this->add_text(lang('index'));
+      $url = new URL('?', cfg('urlvars'));
       $url->set_var('action',   'search');
-      $url->set_var('q',        $this->query);
-      $url->set_var('hs',       0);
-      $url->set_var('forum_id', $this->forum_id);
-      if ($n_indexoffset > 1) {
-        $url->set_var('hs', 0);
-        call_user_func($additem, 1, $url);
-      }
-      if ($n_indexoffset > 2)
-        call_user_func($additem, '...');
-
-      // Print the numbers. Print the active number using another color.
-      for ($i = $n_indexoffset;
-           $i <= $n_indexoffset + $this->n_pages_per_index && $i <= $n_pages;
-           $i++) {
-        if ($i == $activepage)
-          call_user_func($additem, $i);
-        else {
-          $url = clone($url);
-          $url->set_var('hs', ($i - 1) * $this->n_postings_per_page);
-          call_user_func($additem, $i, $url);
-        }
-      }
-
-      // Always show a link to the last page.
-      $url = clone($url);
-      if ($n_indexoffset + $this->n_pages_per_index < $n_pages - 1)
-        call_user_func($additem, '...');
-      if ($n_indexoffset + $this->n_pages_per_index < $n_pages) {
-        $url->set_var('hs', ($n_pages - 1) * $this->n_postings_per_page);
-        call_user_func($additem, $n_pages, $url);
-      }
-
-      // "Newer threads" link.
-      $url = clone($url);
-      call_user_func($additem);
-      if ($activepage > 1) {
-        $url->set_var('hs', ($activepage - 2) * $this->n_postings_per_page);
-        call_user_func($additem, lang("next"), $url);
-      }
-      else
-        call_user_func($additem, lang("next"));
-
-      // "Older threads" link.
-      $url = clone($url);
-      call_user_func($additem);
-      if ($activepage < $n_pages) {
-        $url->set_var('hs', $activepage * $this->n_postings_per_page);
-        call_user_func($additem, lang("prev"), $url);
-      }
-      else
-        call_user_func($additem, lang("prev"), $url);
+      $url->set_var('q',        $_args[query]);
+      $url->set_var('forum_id', $_args[forum_id]);
+      $this->add_index($url,
+                       $_args[n_postings],
+                       $_args[n_postings_per_page],
+                       $_args[n_pages_per_index],
+                       $_args[n_offset]);
     }
   }
 ?>
--- src/plugins/search/indexbar_search_users.class.php
+++ src/plugins/search/indexbar_search_users.class.php
@@ -29,67 +29,19 @@
     // Constructor.
     function IndexBarSearchUsers($_args) {
       $this->Menu();
-      $this->query             = $_args[query];
-      $this->n_users           = $_args[n_users];
-      $this->n_users_per_page  = $_args[n_users_per_page];
-      $this->n_offset          = $_args[n_offset];
-      $this->n_pages_per_index = $_args[n_pages_per_index];
 
-      // Print the "Index" keyword, followed by a separator.
-      $additem = array(&$this, 'add_item');
-      call_user_func($additem, lang("index"));
-
-      // Calculate the total number of pages.
-      $n_pages = ceil($this->n_users / $this->n_users_per_page);
-      if ($n_pages <= 0)
-        $n_pages = 1;
-
-      // Find the selected page's number from the parent with the given offset.
-      $activepage = ceil($this->n_offset / $this->n_users_per_page) + 1;
-
-      // Find the first number to show in the index.
-      $n_indexoffset = 1;
-      if ($activepage > $this->n_pages_per_index / 2)
-        $n_indexoffset = $activepage - ceil($this->n_pages_per_index / 2);
-      if ($n_indexoffset + $this->n_pages_per_index > $n_pages)
-        $n_indexoffset = $n_pages - $this->n_pages_per_index;
-      if ($n_indexoffset < 1)
-        $n_indexoffset = 1;
-
-      // Always show a link to the first page.
-      $url = new URL('?', cfg("urlvars"));
+      // Prints the index (pagination).
+      $this->add_text(lang('index'));
+      $url = new URL('?', cfg('urlvars'));
       $url->set_var('action',      'search');
-      $url->set_var('q',           $this->query);
-      $url->set_var('hs',          0);
+      $url->set_var('q',           $_args[query]);
+      $url->set_var('forum_id',    $_args[forum_id]);
       $url->set_var('user_search', 1);
-      if ($n_indexoffset > 1) {
-        $url->set_var('hs', 0);
-        call_user_func($additem, 1, $url);
-      }
-      if ($n_indexoffset > 2)
-        call_user_func($additem, '...');
-
-      // Print the numbers. Print the active number using another color.
-      for ($i = $n_indexoffset;
-           $i <= $n_indexoffset + $this->n_pages_per_index && $i <= $n_pages;
-           $i++) {
-        if ($i == $activepage)
-          call_user_func($additem, $i);
-        else {
-          $url = clone($url);
-          $url->set_var('hs', ($i - 1) * $this->n_users_per_page);
-          call_user_func($additem, $i, $url);
-        }
-      }
-
-      // Always show a link to the last page.
-      $url = clone($url);
-      if ($n_indexoffset + $this->n_pages_per_index < $n_pages - 1)
-        call_user_func($additem, '...');
-      if ($n_indexoffset + $this->n_pages_per_index < $n_pages) {
-        $url->set_var('hs', ($n_pages - 1) * $this->n_users_per_page);
-        call_user_func($additem, $n_pages, $url);
-      }
+      $this->add_index($url,
+                       $_args[n_users],
+                       $_args[n_users_per_page],
+                       $_args[n_pages_per_index],
+                       $_args[n_offset]);
     }
   }
 ?>

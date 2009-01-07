43f06682ffccd59bc1b61fbc2e317ad7618264ad
knipknap <knipknap>
2009-01-07 07:32:52 +0000

    Improved script for deleting unconfirmed users.

--- delete_unconfirmed_users.sql
+++ delete_unconfirmed_users.sql
@@ -1,2 +1,5 @@
 -- This script deletes all unconfirmed users that are older than 48 hours.
-DELETE FROM freech_user WHERE status=2 AND updated<FROM_UNIXTIME(UNIX_TIMESTAMP() - 60*60*48);
+DELETE FROM freech_user
+WHERE status=2
+AND updated<FROM_UNIXTIME(UNIX_TIMESTAMP() - 60*60*48)
+AND id IN (SELECT user_id FROM freech_posting);

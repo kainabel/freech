b8ab95164749d0cca9b1ba3fe45d794fc127ee7d
knipknap <knipknap>
2009-01-17 23:52:27 +0000

    Update copyright.

--- src/libuseful/string.inc.php
+++ src/libuseful/string.inc.php
@@ -1,7 +1,6 @@
 <?php
   /*
   Copyright (C) 2005 Samuel Abels, <http://debain.org>
-                     Robert Weidlich, <tefinch xenim de>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by

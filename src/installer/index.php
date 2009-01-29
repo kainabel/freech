2f5157f61ba6a08b4e838035c5ec960f6727f413
knipknap <knipknap>
2009-01-29 21:42:14 +0000

    Add initial structure for the installer.

---
+++ src/installer/index.php
@@ -0,0 +1,73 @@
+<?php
+  /*
+  Freech.
+  Copyright (C) 2003-2008 Samuel Abels, <http://debain.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  */
+?>
+<?php
+require_once '../smarty/Smarty.class.php';
+require_once '../adodb/adodb.inc.php';
+include_once '../libuseful/SqlQuery.class.php5';
+include_once 'state.class.php';
+include_once 'statedb.class.php';
+include_once 'step.class.php';
+include_once 'welcome.class.php';
+#include_once 'check_requirements.class.php';
+#include_once 'database_setup.class.php';
+#include_once 'create_default_user.class.php';
+#include_once 'done.class.php';
+
+$steps = array('Welcome',
+               'CheckRequirements',
+               'DatebaseSetup',
+               'CreateDefaultUser',
+               'Done');
+
+$smarty = new Smarty;
+$smarty->template_dir = '.';
+$smarty->compile_dir  = '../data/installer';
+$smarty->cache_dir    = '../data/smarty_cache';
+$smarty->config_dir   = '../data/smarty_configs';
+$smarty->display('header.tmpl');
+
+$statedb      = new StateDB('../data/installer');
+$step_id      = (int)$_GET['step'];
+$prev_step_id = $step_id - 1;
+$state        = $statedb->get($prev_step_id);
+$step_cls     = $steps[$step_id];
+
+if ($prev_step_id >= 0) {
+  $prev_step_cls = $steps[$prev_step_id];
+  $prev_step     = new $prev_step_cls($prev_step_id, $smarty, $state);
+  if ($prev_step->check() && $prev_step->submit()) {
+    $statedb->save($step_id, $state);
+    $step = new $step_cls($step_id, $smarty, $state);
+    $step->show();
+  }
+}
+else {
+  $step = new $step_cls($step_id, $smarty, $state);
+  $step->show();
+}
+
+$smarty->display('footer.tmpl');
+
+// clear entire compile directory
+$smarty->clear_compiled_tpl();
+$smarty->compile_dir = '../data/smarty_templates_c';
+$smarty->clear_compiled_tpl();
+?>
---
+++ src/installer/statedb.class.php
@@ -0,0 +1,63 @@
+<?php
+  /*
+  Freech.
+  Copyright (C) 2003-2008 Samuel Abels, <http://debain.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  */
+?>
+<?php
+class StateDB {
+  function StateDB($_data_dir) {
+    $this->data_dir = $_data_dir;
+    if (!is_dir($this->data_dir))
+      mkdir($this->data_dir);
+  }
+
+
+  function _filename_of($_id) {
+    return sprintf('%s/state%d.cfg', $this->data_dir, (int)$_id);
+  }
+
+
+  /**
+   * Returns the state with the given id. If no such state exists, a new 
+   * state is returned.
+   */
+  function get($_id) {
+    $state     = new State;
+    $state->id = $_id;
+    if (!is_readable($this->_filename_of($_id)))
+      return $state;
+
+    $pairs = parse_ini_file($this->_filename_of($_id));
+    foreach ($pairs as $key => $value)
+      $state->set_attribute($key, $value);
+    return $state;
+  }
+
+
+  /**
+   * Persistently saves the given state under the given id.
+   */
+  function save($_id, $_state) {
+    $_state->id = $_id;
+    $output     = fopen($this->_filename_of($_id), 'w');
+    foreach ($_state->get_attributes() as $key => $value)
+      fwrite($output, sprintf("%s=%s\n", $key, $value));
+    fclose($output);
+  }
+}
+?>

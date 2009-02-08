55c4ccb68f4f75dd98f64cbcb9c64854804dc3b4
knipknap <knipknap>
2009-02-08 22:19:30 +0000

    Minor call tracer improvements.

--- src/services/call_tracer.class.php
+++ src/services/call_tracer.class.php
@@ -31,16 +31,15 @@ class CallTracer {
     $caller1   = $backtrace[3];
     $caller2   = $backtrace[2];
     $dir       = dirname(dirname(__FILE__));
-    $file      = substr($caller2['file'], strlen($dir));
+    $file      = substr($caller2['file'], strlen($dir) + 1);
     $func      = $caller1['function'];
     $line      = $caller2['line'];
 
     // Fake some strace output.
     list($msec, $sec) = explode(' ', microtime());
-    list($foo, $msec) = explode('.', $msec);
     $args    = func_get_args();
     $comment = func_num_args() ? call_user_func_array('sprintf', $args) : '';
-    $msec    = (int)substr($msec, 0, -2);
+    $msec    = (int)($msec * 1000000);
     $str     = "MARK: $file($line): $func, $comment";
     $access  = sprintf("0000 $sec.%06d access(\"$str\", F_OK)\n", $msec);
     if ($this->logfile)

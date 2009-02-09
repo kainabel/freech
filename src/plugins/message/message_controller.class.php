223c6e3e917135745da8558ee2da263a6da7fc25
knipknap <knipknap>
2009-02-09 18:45:46 +0000

    Fix: Message preview.

--- src/plugins/message/message_controller.class.php
+++ src/plugins/message/message_controller.class.php
@@ -91,13 +91,13 @@
       $url->set_var('parent_id', $_parent_id);
 
       $this->clear_all_assign();
+      $this->assign       ('parent_id',   (int)$_parent_id);
       $this->assign_by_ref('onsubmit_js', $this->api->get_js('onsubmit'));
       $this->assign_by_ref('form_html',   $this->api->get_html('form'));
       $this->assign_by_ref('may_quote',   $_may_quote);
-      $this->assign_by_ref('parent_id',   (int)$_parent_id);
-      $this->assign_by_ref('pagetitle', _('Preview'));
-      $this->assign_by_ref('action',    $url->get_string());
-      $this->assign_by_ref('message',   $_message);
+      $this->assign_by_ref('pagetitle',   _('Preview'));
+      $this->assign_by_ref('action',      $url->get_string());
+      $this->assign_by_ref('message',     $_message);
       $this->render(dirname(__FILE__).'/preview.tmpl');
       $this->api->set_title($_message->get_subject());
     }

ff70568c327af3b99c7b90231e189e0ee4796ee2
isorubin <isorubin>
2010-11-24 09:40:02 +0000

    Remove old URL from all files, also from the license head.

--- src/controllers/controller.class.php
+++ src/controllers/controller.class.php
@@ -1,6 +1,6 @@
 <?php
   /*
-  Copyright (C) 2003 Samuel Abels, <http://debain.org>
+  Copyright (C) 2003 Samuel Abels
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
--- src/functions/config.inc.php
+++ src/functions/config.inc.php
@@ -1,7 +1,7 @@
 <?php
   /*
   Freech.
-  Copyright (C) 2003 Samuel Abels, <http://debain.org>
+  Copyright (C) 2003 Samuel Abels
   
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
